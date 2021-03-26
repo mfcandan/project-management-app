2fad78d8519dfbb5026b2e31be576effc8c63e31
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-03-26 15:08:50 +0300

    css added

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -5,9 +5,9 @@ const SignedInLinks = () => {
   return (
     <div>
       <ul className="right">
-        <li><NavLink to='/'>New Project</NavLink></li>
+        <li><NavLink to='/create'>New Project</NavLink></li>
         <li><NavLink to='/'>Log Out</NavLink></li>
-        <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
+        <li><NavLink to='/' className="btn btn-floating pink lighten-1">FC</NavLink></li>
       </ul>
     </div>
   )
